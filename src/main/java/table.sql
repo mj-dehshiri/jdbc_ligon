a41ca57b35011a36f0060dace288cab9cc52a65e
S.L.MJ <mj.dehshiri@gmail.com>
2024-01-19 00:08:22 +0330

    creat table user

---
+++ src/main/java/table.sql
@@ -0,0 +1,8 @@
+Create table IF NOT EXISTS users(
+
+   id SERIAL PRIMARY KEY ,
+   fist_name VARCHAR(255),
+    last_name VARCHAR(255),
+    user_name VARCHAR(255),
+    password VARCHAR(255)
+    );
