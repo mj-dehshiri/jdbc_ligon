d19245db6bc3c95f8773545b520e43428b4c1d49
S.L.MJ <mj.dehshiri@gmail.com>
2024-01-19 00:46:55 +0330

    add new constructor without id

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -15,6 +15,13 @@ public class User {
     this.password = password;
   }
 
+  public User(String firstName, String lastName, String username, String password) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.username = username;
+    this.password = password;
+  }
+
   public int getId() {
     return id;
   }
