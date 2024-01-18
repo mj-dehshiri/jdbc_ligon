651390d5738889001457464fc13d128b55b26f52
S.L.MJ <mj.dehshiri@gmail.com>
2024-01-19 00:32:07 +0330

    add private final scanner

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
 
 public class Menu {
+     private  final Scanner scanner = new Scanner(System.in);
     public void publicMenu(){
-
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("***** WELCOME *****");
         System.out.println("1-signup");
         System.out.println("2- signin");
         System.out.println("3- exit");
-        System.out.println("choose your number");
+        System.out.println("choose your number: ");
 
         int number = scanner.nextInt();
         scanner.nextLine();
