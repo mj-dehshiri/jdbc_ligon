0c0d46608701fe48d0b992366bfd2e0d961c6170
S.L.MJ <mj.dehshiri@gmail.com>
2024-01-19 02:49:41 +0330

    add findbyuser method

--- src/main/java/UserRepository.java
+++ src/main/java/UserRepository.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserRepository {
@@ -18,5 +19,26 @@ public class UserRepository {
         preparedStatement.setString(3,user.getUsername());
         preparedStatement.setString(4,user.getPassword());
         return preparedStatement.executeUpdate();
+    }
+    public User findByUsername(String username) throws SQLException {
+        Connection connection = new JdbcConnection().getConnection();
+
+        String findUser = "SELECT * FROM users WHERE user_name = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(findUser);
+        preparedStatement.setString(1, username);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()){
+            int id = resultSet.getInt("id");
+            String firsname = resultSet.getString("fist_name");
+            String lastname = resultSet.getString("last_name");
+            String fetchUsername = resultSet.getString("user_name");
+            String password = resultSet.getString("password");
+
+            return new User(id,firsname,lastname,fetchUsername,password);
+        }
+        else {
+            return null;
+        }
+
     }
 }
